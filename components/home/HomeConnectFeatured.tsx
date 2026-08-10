e452f3c49bf31d23d967e5c07f23ef2fc1e5318f
Edwin J. Calderon <calderonedwin3@gmail.com>
2026-08-10 17:21:44 -0500

    Center carousel

--- components/home/HomeConnectFeatured.tsx
+++ components/home/HomeConnectFeatured.tsx
@@ -109,7 +109,7 @@ export function HomeConnectFeatured({
       aria-live="polite"
       aria-atomic="true"
     >
-      <div className="home-connect__featured-carousel flex w-full items-center justify-center gap-2 sm:justify-start sm:gap-3 lg:gap-4">
+      <div className="home-connect__featured-carousel flex w-full items-center justify-center gap-2 sm:gap-3 lg:justify-start lg:gap-4">
         <button
           type="button"
           onClick={(event) => {
